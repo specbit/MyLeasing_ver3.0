3ece19f1bef48302769bc0542e4a2860a5f581b0
specbit <nmiguelgomes77@gmail.com>
2022-07-12 18:11:01 +0100

    Add CRUD Owners, Add SeedDb

---
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Document*")]
+        public string Document { get; set; }
+
+        [Display(Name = "Owner Name*")]
+        [NotMapped()] //for not generated to the database using entity framework
+        //[HiddenInput(DisplayValue = false)]  //used when I generated the views automatically
+        public string Fullname => $"{FirstName} {LastName}";
+
+        [Required]
+        [Display(Name = "First Name*")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name*")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+    }
+}
---
+++ MyLeasing.Web/Data/SeedDb.cs
@@ -0,0 +1,54 @@
+﻿using MyLeasing.Web.Data.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLeasing.Web.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+        private Random _random;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+            _random = new Random();
+        }
+
+        public async Task SeedAsync() 
+        {
+            await _context.Database.EnsureCreatedAsync();
+
+            if (!_context.Owners.Any())
+            {
+                AddOwner("Zé", "Boi");
+                AddOwner("Maria", "Couta");
+                AddOwner("Ester", "Feiosa");
+                AddOwner("Keanu", "Reeves");
+                AddOwner("Margot", "Robbie");
+                AddOwner("Drácula", "Boi");
+                AddOwner("Cinha", "Jardim");
+                AddOwner("Tó", "Bacuro");
+                AddOwner("Débora", "Secco");
+                AddOwner("Charlize", "Theron");
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private void AddOwner(string name, string surname)
+        {
+            _context.Owners.Add(new Owner
+            {
+                Document = _random.Next(100000000).ToString(),
+                FirstName = name,
+                LastName = surname,
+                FixedPhone = "92" + _random.Next(10000000).ToString(),
+                CellPhone = "21" + _random.Next(10000000).ToString(),
+            });
+
+            //await _context.AddAsync();
+        }
+    }
+}
